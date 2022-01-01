612cff1de0e70185aeb9b9671e54a953af34519e
botamochi6277 <botamochi6277@icloud.com>
2022-01-01 22:55:29 +0900

    add sweeping

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
 #include "SCServo.h"
 
+#define RX1 21
+#define TX1 25
+
 SMS_STS st;
 
 void setup()
 {
-  // put your setup code here, to run once:
+  Serial.begin(115200); // serial for debugging
+
+  Serial1.begin(1000000, SERIAL_8N1, RX1, TX1);
+  // Serial1.begin(1000000); // serial for servos
+  st.pSerial = &Serial1;
+  delay(1000);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  Serial.println("start sweeping");
+  st.RegWritePosEx(0, 4095, 3400, 50);
+  st.RegWritePosEx(1, 4095, 3400, 50);
+  st.RegWriteAction();
+  delay(1884); //[(P1-P0)/Speed]*1000+[Speed/(Acc*100)]*1000
+
+  st.RegWritePosEx(0, 0, 3400, 50);
+  st.RegWritePosEx(1, 0, 3400, 50);
+  st.RegWriteAction();
+  delay(1884);
 }
