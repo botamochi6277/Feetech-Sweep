30d9a1a8c813388590a9b1ab76cf0b83636d87be
botamochi6277 <botamochi6277@icloud.com>
2023-12-02 09:44:03 +0900

    add m5stack-core

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+
 #include "SCServo.h"
 
-#ifdef M5ATOM
+#ifdef SEEED_XIAO_M0
+#define RX1 7
+#define TX1 6
+#endif
+
+#ifdef ARDUINO_M5Stack_ATOM
 #define RX1 21
 #define TX1 25
+#endif
+
+#ifdef ARDUINO_M5Stack_Core_ESP32
+// CORE2??
+#define RX1 19
+#define TX1 27
 #endif
 
 SMS_STS st;
@@ -13,28 +25,24 @@ short min_sweep = 0;
 unsigned short speed = 3400;
 unsigned char acc = 50;
 
-void setup()
-{
+void setup() {
   // LED_BUILTIN;
-  Serial.begin(115200); // serial for debugging
-#ifdef M5ATOM
+  Serial.begin(115200);  // serial for debugging
   Serial1.begin(1000000, SERIAL_8N1, RX1, TX1);
-#endif
-#ifdef XIAO
-  Serial1.begin(1000000); // serial for servos
-#endif
 
   st.pSerial = &Serial1;
   delay(1000);
+  if (!Serial1.available()) {
+    Serial.println("Serial1 is unavailable");
+  }
 }
 
-void loop()
-{
+void loop() {
   Serial.println("start sweeping");
   st.RegWritePosEx(0, max_sweep, speed, acc);
   st.RegWritePosEx(1, max_sweep, speed, acc);
   st.RegWriteAction();
-  delay(1884); //[(P1-P0)/Speed]*1000+[Speed/(Acc*100)]*1000
+  delay(1884);  //[(P1-P0)/Speed]*1000+[Speed/(Acc*100)]*1000
 
   st.RegWritePosEx(0, min_sweep, speed, acc);
   st.RegWritePosEx(1, min_sweep, speed, acc);
